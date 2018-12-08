1d72d97c9b6ea97b60fabf819bff9011d3e7f865
creanost <creanost@gmail.com>
2018-12-07 21:37:01 -0600

    initial commit

---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float thrust;
+
+	
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	void FixedUpdate () {
+		if(Input.GetKey("up")){
+			rb.AddForce(transform.forward * thrust);
+		}
+
+		if(Input.GetKey("down")){
+			rb.AddForce((transform.forward * thrust) * -1);
+		}
+	}
+}
